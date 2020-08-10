83b14c0d56f26b24c67de083ddd91f5c7630b52f
ako-21 <akeemogle@gmail.com>
2020-08-10 11:53:40 -0400

    Added 'name' virtual to user model Created a name virtual to display all characters before the @ in the email in the front end Welcome Message

--- app/models/user.js
+++ app/models/user.js
@@ -18,8 +18,14 @@ const userSchema = new mongoose.Schema({
     transform: (_doc, user) => {
       delete user.hashedPassword
       return user
-    }
+    },
+    virtuals: true
   }
+})
+
+userSchema.virtual('name').get(function () {
+  const index = this.email.indexOf('@')
+  return this.email.substring(0, index)
 })
 
 module.exports = mongoose.model('User', userSchema)
